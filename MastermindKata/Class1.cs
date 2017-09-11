323720fd9852fb807a9f52f952d111fe537730ea
Nick Ye <30612557+nickyemyob@users.noreply.github.com>
2017-09-11 17:14:32 +1200

    Initial commit

---
+++ MastermindKata/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastermindKata
+{
+    public class Class1
+    {
+    }
+}
