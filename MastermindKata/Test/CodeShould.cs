9314a2ea20e7bf3bd861669539718726b58c02bf
Viet Anh Tran MYOB <viet.anhtran@myob.com>
2017-09-17 15:57:10 +1200

    Unit test for Code class to compare code length

---
+++ MastermindKata/Test/CodeShould.cs
@@ -0,0 +1,18 @@
+﻿using MastermindKata.Logic;
+using NUnit.Framework;
+
+namespace MastermindKata.Test
+{
+    [TestFixture]
+    public class CodeShould
+    {
+        [Test]
+        public void ReturnCorrectLengthCode()
+        {
+            var code = new Code();
+            var randomCode = code.Generate();
+            var defaultCodeLength = 4;
+            Assert.AreEqual(defaultCodeLength, randomCode.Length);
+        }
+    }
+}
