47b6a4c5291498a35f1b8d510146aa6c22976973
Viet Anh Tran MYOB <viet.anhtran@myob.com>
2017-09-17 15:59:22 +1200

    Implementation for Code to generate random code

---
+++ MastermindKata/Logic/Code.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastermindKata.Logic
+{
+    public class Code
+    {
+        private static readonly Random Random = new Random();
+        private readonly string _defaultColor = "rgyc";
+        private readonly int _codeLength = 4;
+
+        public string Generate()
+        {
+            return new string(Enumerable.Repeat(_defaultColor, _codeLength)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+    }
+}
