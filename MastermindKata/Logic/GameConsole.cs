e1e5dd9056ee6dd35107cbe4ed9155475a774fb9
Viet Anh Tran MYOB <viet.anhtran@myob.com>
2017-09-18 17:01:31 +1200

    Add console to play the game

---
+++ MastermindKata/Logic/GameConsole.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace MastermindKata.Logic
+{
+    public static class GameConsole
+    {
+        static void Main()
+        {
+            var mastermind = new Mastermind();
+            var code = new Code();
+            var guess = "";
+            var response = "The mark was: ";
+            
+            while (guess != "stop")
+            {
+                Console.WriteLine("Welcome to Mastermind! Please insert your guess (eg: r,g,y,c)");
+                guess = Console.ReadLine();
+                if(guess == "stop") return;
+                var mark = mastermind.ReturnMark(code.Generate(), guess);
+                Console.WriteLine(response+ mark);
+            }
+            Console.ReadKey();
+        }
+    }
+}
