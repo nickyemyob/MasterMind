bb0c7e5524a95f8d8675d892b9c2a1d67ec871a4
Viet Anh Tran MYOB <viet.anhtran@myob.com>
2017-09-18 17:20:39 +1200

    Unit test and implementation for GuessValidator

---
+++ MastermindKata/Logic/GuessValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace MastermindKata.Logic
+{
+    internal class GuessValidator
+    {
+        public string CheckForComma(string guess)
+        {
+            return !guess.Contains(",") ? "Guess should contain commas between colours." : string.Empty;
+        }
+    }
+}
---
+++ MastermindKata/Test/GuessValidatorShould.cs
@@ -0,0 +1,18 @@
+﻿using MastermindKata.Logic;
+using NUnit.Framework;
+
+namespace MastermindKata.Test
+{
+    [TestFixture]
+    class GuessValidatorShould
+    {
+        [Test]
+        public void ReturnErrorMessageWhenUserGuessDoesNotContainCommas()
+        {
+            var guessValidator = new GuessValidator();
+            const string expectedErrorMessage = "Guess should contain commas between colours.";
+            var actualErrorMessage = guessValidator.CheckForComma("rgdf");
+            Assert.AreEqual(expectedErrorMessage, actualErrorMessage);
+        } 
+    }
+}
