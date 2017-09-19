0fcccbac0f816a4e70fdf87d17d307f100b69a2d
Viet Anh Tran MYOB <viet.anhtran@myob.com>
2017-09-19 20:31:24 +1200

    Create Mock for GameConfigSerialiser to mock in unit test

---
+++ MastermindKata/Mock/MockGameConfigSerialiser.cs
@@ -0,0 +1,18 @@
+﻿using MastermindKata.DTO;
+using MastermindKata.Interface;
+
+namespace MastermindKata.Mock
+{
+    public class MockGameConfigSerialiser : IGameConfigSerialiser
+    {
+        public GameConfig ConvertFromXml(string xmlFilePath)
+        {
+            return new GameConfig(){DefaultColour = "rgycw"};
+        }
+
+        public void Save(string filemname, GameConfig obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- MastermindKata/Test/CodeShould.cs
+++ MastermindKata/Test/CodeShould.cs
@@ -1,4 +1,5 @@
 ﻿using MastermindKata.Logic;
+using MastermindKata.Mock;
 using NUnit.Framework;
 
 namespace MastermindKata.Test
@@ -9,7 +10,7 @@ namespace MastermindKata.Test
         [Test]
         public void ReturnCorrectLengthCode()
         {
-            var code = new Code();
+            var code = new Code(new GameConfigService(new MockGameConfigSerialiser()));
             var randomCode = code.Generate();
             var lengthOfCodeWithComma = 7;
             Assert.AreEqual(lengthOfCodeWithComma, randomCode.Length);
